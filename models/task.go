b979edabda5d5abb9d8113c470832751ee48aa2e
Siyovush <paraparadox@protonmail.com>
2023-03-17 00:25:28 +0500

    Prepare module layout

---
+++ models/task.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Task struct {
+	ID        uint           `json:"id" gorm:"primary_key"`
+	Text      string         `json:"text"`
+	Status    string         `json:"status"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
