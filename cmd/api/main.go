88f930a2f0bbe106e4546635fbe286d44d048066
Siyovush <paraparadox@protonmail.com>
2023-03-17 00:30:13 +0500

    Get gin framework

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
